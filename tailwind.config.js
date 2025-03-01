d7afa802dd0889a8533cbda8261dcbd838d07864
jomanuelrp <jomanuelrp@gmail.com>
2025-03-01 09:57:04 -0600

    alguna arreglo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import presets from '.vendor/filament/support/tailwind.config.preset';
+import presets from ".vendor/filament/support/tailwind.config.preset";
 
 module.exports = {
-  presets: [presets],
-  content: [
-    './app/Filament/**/*.php',
-    './resources/views/filament/**/*.blade.php',
-    './vendor/filament/**/*.blade.php',],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    presets: [presets],
+    content: [
+        "./app/Filament/**/*.php",
+        "./resources/views/filament/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
